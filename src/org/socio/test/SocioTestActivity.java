2ab5ab08b86b26b670e4a5937d942cf212f87760
Mikhail S. Pobolovets <styx.mp@gmail.com>
2011-10-24 16:35:35 +0300

    Initial commit
    Signed-off-by: Mikhail S. Pobolovets <styx.mp@gmail.com>

---
+++ src/org/socio/test/SocioTestActivity.java
@@ -0,0 +1,13 @@
+package org.socio.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SocioTestActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
